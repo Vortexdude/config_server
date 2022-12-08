84a0c4f95b6b6fc40c09ef9e7a366a1bd020e9f2
Nitin Namdev <nitin.namdev@intelliswift.com>
2022-12-08 23:50:20 +0530

    addin the new feature of dump the passsword

--- main.sh
+++ main.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
-set -ex
+# set -ex
+
+function dump_event(){ 
+  echo "[${1}] ${2}" 
+  [ ${ignore_errors} ] || exit 1
+}
+
+function usage(){
+echo "Please use as $0 user1 user2 user3 ..."
+}
+
 debug_level=0
 product_name=srelia
 logdir=/var/log/${product_name}/
@@ -12,13 +22,19 @@ connection=local
 ignore_errors=true
 role=create_users
 
+# ( set -o posix ; set ) | less
+
+# for info in product_name logdir clone_path clone_url branch_name
+# do
+#   dump_event "Info" "${info} ${info}"
+# done
+
 if [[ "${debug_level}" -eq 0 ]]; then output="/dev/null"; else output=">${logdir}/error.log"; fi
 
 
 function dump_event(){ 
-  echo " ${1} ${2}" 
-  [ ${ignore_errors} ] && status=0 || status=1
-  exit ${status}
+  echo "[${1}] ${2}" 
+  [ ${ignore_errors} ] || exit 1
 }
 
 function usage(){
@@ -26,7 +42,7 @@ echo "Please use as $0 user1 user2 user3 ..."
 }
 
 function clone_repo(){
-  git clone -b ${branch_name} ${clone_url} ${clone_path}
+  git clone -b ${branch_name} ${clone_url} ${clone_path} 2>${output} || dump_event "Error" "Can't able to clone the Repo check the logs at ${log_dir}"
 }
 
 function required_directories(){
@@ -34,22 +50,23 @@ function required_directories(){
   if [ -d ${clone_path} ]; then dump_event "Warning" "Directory Exist" && rm -rf ${clone_path} ${logdir}; else  mkdir -p ${clone_path} ${logdir}; fi
 }
 
+# exit from usages
+if [[ "${#}" -lt 1 ]]; then usage && exit 1; fi
+
 # installing ansible 
 dump_event "Info" "Installing ansible"
 if [[ "${os_version}" -eq "Ubuntu" ]]; then apt install ansible jq -y 2>${output} >/dev/null; else yum install ansible jq -y 2>${output} >/dev/null; fi
-
-# exit from usages
-if [[ "${#}" -lt 1 ]]; then usage && exit 1; fi
 
 # set the defualt permissions
 required_directories
 
 #cloning github repo
-clone_repo && dump_event "Info" "Cloning the repo ${clone_url} in the ${branch_name} branch " || dump_event "Error" "Can't able to Clone the repo"
+clone_repo && dump_event "Info" "Cloning the repo ${clone_url} in the ${branch_name} branch "
 
 # overwrring defaul variables
 default_variable_file="${clone_path}/ansible/roles/${role}/defaults/main.yml"
-echo "users: " >${default_variable_file}
+echo "password_file_path: ${clone_path}" >${default_variable_file} 
+echo "users: " >>${default_variable_file}
 for name in "${@}"
 do
 cat << EOF >> ${default_variable_file}
@@ -58,9 +75,9 @@ EOF
 done
 
 # run the ansible playbook
-echo "**** Running Ansible playbook"
-ansible-playbook ${clone_path}/ansible/${role}.yml -i ${server}, -c ${connection}"
-if [[ "${?}" -eq 0 ]]; then echo "**** Succesfully created ${#} users - ${@}" ; else "****  There might be an issue" && exit 1; fi
-
-echo "**** Deleting temprary files"
-#rm -rf ${clone_path}
+dump_event "Info" "Running Ansible playbook"
+ansible-playbook ${clone_path}/ansible/${role}.yml -i ${server}, -c ${connection} 
+[ "${?}" -eq 0 ] && dump_event "Info" "Succesfully created ${#} users - ${@}" ||  dump_event "Error" "There might be an issue with playbook "
+dump_event "Info" "$(cat ${clone_path}/password.txt)"
+# Deleting temprary files
+[ -d ${clone_path} ] && dump_event "Info" "Deleting temprary files" && rm -rf ${clone_path} || dump_event "Error" "Permission denied"
